9d95e14c556e015f2d3418b01f938a2449199e61
Christian Laggui <claggui27@gmail.com>
2022-08-29 15:31:21 +0000

    Changes to be committed: 	modified:   createlogin.php 	modified:   js/app.js 	modified:   newUser.php

--- createlogin.php
+++ createlogin.php
@@ -2,37 +2,43 @@
     session_start();
     require "db_conn.php";
 
-    if(isset($_POST['user']) && isset($_POST['password'])){
-
-        function validate($data){
-            $data = trim($data);
-            return $data;
-        }
+    function validate($data){
+        $data = trim($data);
+        return $data;
     }
-
-    $uname = validate($_POST['user']);
-    $pass = validate($_POST['password']);
     
-    try{
-        $sql = "INSERT INTO users (username, password, email)
-                    values ('$uname', '$pass', '$email')";
+    $uname = validate($_POST['uname']);
+    $pass = validate($_POST['password']);
 
-        $statement = $conn->query($sql);
-        $success = $statement->execute();
-    }catch(PDOExceoption $error){
+    $data = [
+	'uname' => $uname,
+	'pass' => $pass
+    ];
+
+//	header("Location: newUser.php?error=createlogin reached");
+
+    try{
+        $sql = "INSERT INTO users (username, password) VALUES (:uname, :pass)";
+
+        $statement = $conn->prepare($sql);
+        $success = $statement->execute($data);
+    }
+    catch(PDOExceoption $error){
         header("Location: newUser.php?error=Unable to create account:".$error->getMessage());
         exit();
     }
     if($success){
         echo "Redirecting...";
         $_SESSION['username'] = $uname;
-        $_SESSION['name'] = $name;
+      //  $_SESSION['name'] = $name;
         $_SESSION['password'] = $pass;
         header("Location: home.php");
-        exit();
+	
+//	header("Location: newUser.php?error=Connected");
+	exit();
     }
     else{
         header("Location: newUser.php?error=Unable to create account.")
-    }
-
+    	exit();
+   }
 ?>
--- newUser.php
+++ newUser.php
@@ -5,33 +5,36 @@
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-        <section>
+	<section>
+	    <h1>Sign Up</h1>
+	    <h4>Create an Account to get started with SubShare!</h4>
             <form action="createlogin.php" method="post" id="signup">
                 <div class="form-field">
                     <label for="user"> Username: </label>
-                    <input type="text" id="user" name="uname" placeholder="Username" required>
+		    <input type="text" id="user" name="uname" placeholder="Username">
                     <alert></alert>
                 </div>
                 <div class="form-field">
                     <label for="pass"> Password (5 Characters minimum): </label>
-                    <input type="password" id="pass" name="password" placeholder="!1abc" required>
+                    <input type="password" id="pass" name="password" placeholder="!1abc">
                     <alert></alert>
                 </div>
                 <div class="form-field">
                     <label for="retype"> Retype Password: </label>
-                    <input type="password" id="retype" required> 
+                    <input type="password" id="retype"> 
                     <alert></alert>
                 </div>
                 <div class="form-field">
                     <label for="email"> Email Address: </label>
-                    <input type="text"  id="emai" name="email" placeholder="example@mail.com" required>
+                    <input type="text"  id="email" name="email" placeholder="example@mail.com">
                     <alert></alert>
                 </div>
 
                 <button type="submit"> Create Account </button>
             </form>
         </section>
-        <alert></alert>
+	<alert></alert>
+	<p class="error"> <?php echo $_GET['error']; ?></p>
         <script src="js/app.js"></script>
     </body>
 </html>
