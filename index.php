5fadee588d04d806217910e7b3b466eb4532fec6
Christian Laggui <claggui27@gmail.com>
2022-08-30 14:10:14 -0400

    Changes to be committed: 	modified:   index.php

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php echo file_get_contents("html/header.html"); ?>
 
+<div class='welcome-banner'>
+    <div class='welcome-content'>
+        <welcome>
+            <h2> Welcome to SubShare! </h2>
+            <br>
+            <p> Easily coordinate sharing subscriptions with your family!</p>
+            <a href='home.php'>
+                <p> Get Started Now! </p>
+            </a>
+        </welcome>
+    </div>
+</div>
 <div class='content'>
-    <welcome>
-        <h2> Welcome to SubShare! </h2>
-        <br>
-        <p> Easily coordinate sharing subscriptions with your family!</p>
-    </welcome>
     <main>
-        <a href='home.php'>
-            <p> Get Started Now! </p>
-        </a>
+        
     </main>
 </div>
 <?php echo file_get_contents("html/footer.html"); ?>
