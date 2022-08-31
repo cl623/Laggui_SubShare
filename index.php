b27b9e71ad9d7ca3a027a74154ebede0f3272f66
Christian Laggui <claggui27@gmail.com>
2022-08-31 16:26:17 -0400

    second parallax

--- index.php
+++ index.php
@@ -10,9 +10,18 @@
         <button class="btn" href='home.php'>Get Started Now!</button>
     </div>
 </div>
-<div class='content'>
-    <main>
-     
-    </main>
+<div class='grid grid-pad'>
+    <!-- split the div -->
+    <div class='col-1-5'> <!-- left 20% -->
+        <h3> About </h3>
+    </div>
+    <div class='col-9-12'> <!-- right 75% -->
+        <p>Subshare is a project to help coordinate splitting bills for subscriptions! 
+        Just select the subscription, price, and participants</p>
+    </div>
+</div>
+<div class= 'parallax2'>
+    <div>
+    </div>
 </div>
 <?php echo file_get_contents("html/footer.html"); ?>
