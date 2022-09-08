31d83a2a25ccb32c6f94634e420055d6baa572a4
Christian Laggui <claggui27@gmail.com>
2022-09-08 15:26:42 -0400

    updating the login

--- login.php
+++ login.php
@@ -6,39 +6,39 @@
     </head>
     <body>
         <header>
-            <div class=title>
-                <h1>ShareSub</h1>
-                <h3>Welcome to ShareSub! Coordinate sharing subscription based platforms with your friends and family easily!</h3>
-            </div>
+            <h1>ShareSub</h1>
+            <h3>Welcome to ShareSub! Coordinate sharing subscription based platforms with your friends and family easily!</h3>
         </header>
-        <main>
-            <section>
-                <h2><b>Login</b></h2>
-                <form action="performlogin.php" method="post">
-                    <p>Username:</p>
-                    <input type="text" name="uname" placeholder="Username">
-                    <br>
-                    <p>Password:</p>
-                    <input type="text" name="password" placeholder="!1Abcd">
-                    <br>
-                    <br>
-                    <button type="submit"> Login </button>
-                </form>
-            </section>
-            <section>
-                <form action="newUser.php" method="post">
-                    <p>New to SubShare?
+        <div class="centerbox">
+            <main>
+                <section>
+                    <h2><b>Login</b></h2>
+                    <form action="performlogin.php" method="post">
+                        <p>Username:</p>
+                        <input type="text" name="uname" placeholder="Username">
                         <br>
-                        Create an account here!
-                    </p>
-                    <button href="newUser.php"> Sign Up </button>
-                </form>
+                        <p>Password:</p>
+                        <input type="text" name="password" placeholder="!1Abcd">
+                        <br>
+                        <br>
+                        <button type="submit"> Login </button>
+                    </form>
+                </section>
+                <section>
+                    <form action="newUser.php" method="post">
+                        <p>New to SubShare?
+                            <br>
+                            Create an account here!
+                        </p>
+                        <button href="newUser.php"> Sign Up </button>
+                    </form>
+                </section>
+            </main>
+            <section>
+                <?php if(isset($_GET['error'])){ ?>
+                    <p class="error"> <?php echo $_GET['error']; ?></p>
+                <?php } ?>
             </section>
-        </main>
-        <section>
-            <?php if(isset($_GET['error'])){ ?>
-                <p class="error"> <?php echo $_GET['error']; ?></p>
-            <?php } ?>
-        </section>
+        </div>
     </body>
 </html>
