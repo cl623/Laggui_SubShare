7ea89aebc4b3492eef403b15de0d2ab6040d417b
Christian Laggui <claggui27@gmail.com>
2022-09-09 12:12:26 -0400

    login background updated

--- login.php
+++ login.php
@@ -5,39 +5,41 @@
         <link rel="stylesheet" type="text/css" href="./css/style.css">
     </head>
     <body>
-	<div>
-	    <h1>ShareSub</h1>
-            <h3>Welcome to ShareSub! Coordinate sharing subscription based platforms with your friends and family easily!</h3>
-        </div>
-        <div class="centerbox">
-            <div class="login">
-                <h2><b>Login</b></h2>
-                <form action="performlogin.php" method="post">
-                    <p>Username:</p>
-                    <input type="text" name="uname" placeholder="Username">
-                    <br>
-                    <p>Password:</p>
-                    <input type="text" name="password" placeholder="!1Abcd">
-                    <br>
-                    <br>
-                    <button type="submit"> Login </button>
-                </form>
-            </div>
-        </div>
-        <div class="centerbox">
+        <div class=login-body>
             <div>
-                <form action="newUser.php" method="post">
-                    <p>New to SubShare?
+                <h1>ShareSub</h1>
+                <h3>Welcome to ShareSub! Coordinate sharing subscription based platforms with your friends and family easily!</h3>
+            </div>
+            <div class="centerbox">
+                <div class="login">
+                    <h2><b>Login</b></h2>
+                    <form action="performlogin.php" method="post">
+                        <p>Username:</p>
+                        <input type="text" name="uname" placeholder="Username">
                         <br>
-                        Create an account here!
-                    </p>
-                    <button href="newUser.php"> Sign Up </button>
-                </form>
+                        <p>Password:</p>
+                        <input type="text" name="password" placeholder="!1Abcd">
+                        <br>
+                        <br>
+                        <button type="submit"> Login </button>
+                    </form>
+                </div>
             </div>
-            <div>
-                <?php if(isset($_GET['error'])){ ?>
-                    <p class="error"> <?php echo $_GET['error']; ?></p>
-                <?php } ?>
+            <div class="centerbox">
+                <div>
+                    <form action="newUser.php" method="post">
+                        <p>New to SubShare?
+                            <br>
+                            Create an account here!
+                        </p>
+                        <button href="newUser.php"> Sign Up </button>
+                    </form>
+                </div>
+                <div>
+                    <?php if(isset($_GET['error'])){ ?>
+                        <p class="error"> <?php echo $_GET['error']; ?></p>
+                    <?php } ?>
+                </div>
             </div>
         </div>
     </body>
