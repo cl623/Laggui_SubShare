3eacb48093b496fc397fc4ef3892b695b2715432
Christian Laggui <claggui27@gmail.com>
2022-09-16 12:09:04 -0400

    Changes to be committed: 	new file:   common/footer.php 	new file:   common/header.php

---
+++ common/footer.php
@@ -0,0 +1,8 @@
+        </div>
+        <footer>
+            <p>Created by Christian Laggui</p>
+           	<div class='bar'></div>
+	    <p>Contact me at: <a href="mailto:claggui27@gmail.com">claggui27@gmail.com</a></p>
+        </footer>
+    </body>
+</html>
---
+++ common/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="../css/icons.css">
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+        <title><?php echo $title;?></title>
+    </head>
+    <body>
+        <header>
+            <nav id="navigation">
+                <a href="home.php">Home</a>
+                <a href="friends.php">Friends</a>
+                <a href="subscriptions.php">Subscriptions</a>
+                <a href="bills.php">Bills</a>
+            </nav>
+        </header>
+        <div id="contents">
